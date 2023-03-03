28ca196d5208160d93d01487352eed277cf3884f
lucas petruceli mattos <lucaspmtt@gmail.com>
2023-03-03 19:35:29 -0300

    add config cors

--- src/main/java/com/api/medicando/main/MainApplication.java
+++ src/main/java/com/api/medicando/main/MainApplication.java
@@ -3,11 +3,13 @@ package com.api.medicando.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.api.medicando.main.db.repositories")
 @EntityScan("com.api.medicando.main.db.tables")
+@Import(Webconfig.class)
 public class MainApplication {
 
 	public static void main(String[] args) {
